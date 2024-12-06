35f97c67ced4e21e896918f107faf08653342f24
kny <162992055+chwwy@users.noreply.github.com>
2024-12-07 01:40:57 +0700

    implement login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Kemenkes from "../assets/kemenkes.png";
 import { useEffect, useState } from "react";
 
@@ -8,43 +8,33 @@ import { useEffect, useState } from "react";
 const LoginPage = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
 
-	/*
-	api : http://localhost:3987/admin/create
+	const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3987/admin/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                }),
+            });
+            const data = await response.json();
 
-	payload request:
-		{
-			"username" : "user test",
-			"password" : "telkomUniversity@kemenkes.61224"
-		}
-	*/
-	const handleSubmit = async () => {
-		try {
-
-			const response = await fetch("http://localhost:3987/admin/create", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					username,
-					password,
-				}),
-			});
-
-			const data = await response.json();
-			console.log(data);
-
-		} catch (error) {
-			console.log(error);
-		}
-
-	}
-
-
-
-
+            if (response.ok) {
+                navigate("/dashboard");
+            } else {
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+	};
 
 	return (
 		<div className="flex items-center justify-center h-screen w-screen bg-gray-100">
@@ -60,61 +50,49 @@ const LoginPage = () => {
 					</p>
 				</div>
 
-				<form className="space-y-4">
-					<div>
-						<label
-							htmlFor="username"
-							className="block text-md font-medium text-gray-700"
-						>
-							Username
-						</label>
-						<input
-							type="text"
-							id="username"
-							className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
-							placeholder="Username"
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-					</div>
+				<form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label
+                        htmlFor="username"
+                        className="block text-md font-medium text-gray-700"
+                        >
+                        Username
+                        </label>
+                        <input
+                        type="text"
+                        id="username"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        />
+                    </div>
 
-					<div>
-						<label
-							htmlFor="password"
-							className="block text-md font-medium text-gray-700"
-						>
-							Kata Sandi
-						</label>
-						<input
-							type="password"
-							id="password"
-							className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
-							placeholder="Password"
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-					</div>
+                    <div>
+                        <label
+                        htmlFor="password"
+                        className="block text-md font-medium text-gray-700"
+                        >
+                        Kata Sandi
+                        </label>
+                        <input
+                        type="password"
+                        id="password"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        />
+                    </div>
 
-					{/* <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="remember-me"
-              className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
-            />
-            <label
-              htmlFor="remember-me"
-              className="ml-2 block text-sm text-gray-900"
-            >
-              Ingat saya
-            </label>
-          </div> */}
-					<button
-						type="submit"
-						className="w-full py-2 px-4 bg-teal-600 text-white rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
-						onClick={handleSubmit}
-					>
-						Log In
-
-
-					</button>
+                    <button
+                        type="submit"
+                        className="w-full py-2 px-4 bg-teal-600 text-white rounded-md shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                    >
+                        Log In
+                    </button>
 					<p className="text-sm text-center"><Link to="/dashboard">Masuk tanpa login</Link></p>
 				</form>
 			</div>
